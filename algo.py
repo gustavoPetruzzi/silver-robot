1c14b7376df8ac22f90482745f20b0f61be8b975
mavocado3 <gustavopetruzzi19@gmail.com>
2017-03-17 17:41:13 -0300

    asdasda

--- algo.py
+++ algo.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import cotizacion
 import urllib2
 
+# TODO: Ver como hacer si el usuario pone otro formato de fecha 
 fecha = raw_input('Ingrese una fecha: ')
 cotizacion.validarFecha(fecha)
 
@@ -15,17 +16,18 @@ table = soup.find('tbody')
 rows = table.find_all('tr')
 for tr in rows:
 #Aca creo la lista con las cotizaciones.
-    cols = tr.find_all('td')
-    if cols[3].text.strip() == fecha:
-        print 'son iguales'
-    p = cols[0].text.strip()
-    d = cols[1].text.strip()
-    e = cols[2].text.strip()
-    f = cols[3].text.strip()
-    #print(p)
-    #print(d)
-    #print(e)
-    #print(f)
+	cols = tr.find_all('td')
+	p = cols[0].text.strip()
+	d = cols[1].text.strip()
+	e = cols[2].text.strip()
+	f = cols[3].text.strip()
+	if fecha == f:
+		cotizacionDia = cotizacion.cotizacion(f,e,d)	
+		break;
+
+print cotizacionDia.fecha
+print cotizacionDia.compra
+print cotizacionDia.venta
 
 
 
--- test.py
+++ test.py
@@ -2,4 +2,7 @@
 
 import cotizacion
 
-print cotizacion.validarFecha("1-1-1991")
+print cotizacion.validarFecha("1/1/1991")
+#Pruebo si me instancia bien el objeto
+cotizacionDia = cotizacion.cotizacion("21/11/1991", 15.49, 19)
+print isinstance(cotizacionDia, cotizacion.cotizacion)
